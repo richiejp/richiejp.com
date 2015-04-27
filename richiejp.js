74c9bc247c429fb650e018a1119efd5839ed0c75
Richard Palethorpe <richiejp@f-m.fm>
2015-04-27 12:25:56 +0100

    Made the automaton move and fixed stuff

--- richiejp.js
+++ richiejp.js
@@ -29,7 +29,8 @@ var cubeGrid = ( function(
       colour: colour,
       x: x,
       y: y,
-      orientation: [0, 1]
+      orientation: [0, 1],
+      sceneId: null
     };
     me.automatonCreatedCB( a );
     return a;
@@ -51,6 +52,16 @@ var cubeGrid = ( function(
     return getNode( a.x, a.y );
   };
 
+  var getAutomatons = function( node ) {
+    var autos = node.edgeSquares.map(
+	function( s ) { return s.automaton; }
+    );
+    if( autos.length < 1 && node.coveringSquare != null) {
+      autos.push( node.coveringSquare.automaton );
+    }
+    return autos;
+  };
+
   var turnLeft = function( orientation ) {
     var o = [];
     o.push( orientation[1] );
@@ -66,7 +77,13 @@ var cubeGrid = ( function(
   };
 
   var getAdjacentNode = function(a, o) {
-    return getNode( a.x + o[0], a.y + o[1] );
+    var ax = a.x + o[0];
+    var ay = a.y + o[1];
+    if( ax < xcount && ay < ycount ) {
+      return getNode( ax, ay );
+    } else {
+      return null;
+    }
   };
 
   var getAdjacentNodes = function(a) {
@@ -83,16 +100,21 @@ var cubeGrid = ( function(
     return nodes;
   };
 
+  var isSingleEdge = function( node ) {
+    return node.edgeSquares.length === 1;
+  };
+
   var isDoubleEdge = function( node ) {
-    return node.edgeSquares.count > 1;
+    return node.edgeSquares.length > 1;
   };
 
   var canTrace = function( toNode, fromNode ) {
-    if( toNode.coveringSquare !== null ){
+    if( toNode === null
+	|| toNode.coveringSquare !== null 
+	|| ( isDoubleEdge( toNode ) 
+	     && isDoubleEdge( fromNode ) ) ) {
       return false;
-    } else if( isDoubleEdge( toNode ) && isDoubleEdge( fromNode ) ) { 
-      return false;
-    }
+    } 
     return true;
   };
 
@@ -126,7 +148,7 @@ var cubeGrid = ( function(
   var traceSquare = function( a ) { 
     var startPos = [ a.x, a.y, 
       a.orientation[ 0 ], a.orientation[ 1 ] ];
-    var topLeft = null;
+    var topLeft = [ ];
     var traceNodes = [ ];
     var size = getLargestNieghbor( a )
       * getRandom( minChildProportion, maxChildProportion );
@@ -144,10 +166,12 @@ var cubeGrid = ( function(
       a.orientation = o;
       topLeft = startPos.slice(0, 2);
 
-      for( var c = 0; side < 4; c++ ) {
-	if(c > size ) {
+      for( var c = 1; side < 4; c++ ) {
+	if(c >= size ) {
 	  side++;
-	  a.orientation = turnRight( o );
+	  o = turnRight( o );
+	  a.orientation = o;
+	  c = 1;
 	}
 	current = getCurrentNode( a );
 	traceNodes.push( current );
@@ -171,8 +195,8 @@ var cubeGrid = ( function(
 
     var square = createSquare( a, topLeft, size );
 
-    for( var x = startPos[ 0 ] + 1; x < size; x++ ) {
-      for( var y = startPos[ 1 ] + 1; y < size; y++ ) {
+    for( var x = topLeft[ 0 ] + 1; x < topLeft[ 0 ] + size - 1; x++ ) {
+      for( var y = topLeft[ 1 ] + 1; y < topLeft[ 1 ] + size - 1; y++ ) {
        	getNode( x, y ).coveringSquare = square;
       }
     }
@@ -184,10 +208,65 @@ var cubeGrid = ( function(
     me.createdSquareCB( square );
   }
 
-  var advanceAutomaton = function(a) { }
+  var okAdvance = function( a, n ) {
+    return isDoubleEdge( n ) && getAutomatons( n ).some( 
+	function( na ) { return na === a; }
+    );
+  }
 
-  me.doStep = function() {
-    
+  var niceAdvance = function( a, n ) {
+    return isSingleEdge( n ) && 
+      getAutomatons( n ).pop() === a;
+  }
+
+  var advanceAutomaton = function(a) {
+    var o = a.orientation;
+    var directions = [
+      turnLeft( o ),
+      o,
+      turnRight( o )
+    ];
+    var possibilities = [ ];
+    directions.forEach( function( d ) {
+      var an = getAdjacentNode( a, d );
+      if( an !== null ) {
+	  possibilities.push( {
+	  n: an,
+	  o: d
+	} );
+      }
+    } );
+
+    var success = possibilities.some( function( t ) {
+      if( niceAdvance( a, t.n ) ) {
+	a.orientation = t.o;
+	return true;
+      }
+      return false;
+    } );
+
+    if( !success ) {
+      success = possibilities.some( function( t ) {
+	if( okAdvance( a, t.n ) ) {
+	  a.orientation = t.o;
+	  return true;
+	}
+	return false;
+      } );
+    }
+
+    if( success ) {
+      move( a );
+      me.automatonMovedCB( a );
+    } else {
+      console.error("Automaton could not find path!");
+    }
+  }
+
+  me.doSim = function() {
+    automatons.map( function( a ) {
+      advanceAutomaton( a );
+    } );
   }
 
   me.automatonCreatedCB = function ( automaton ) { }
@@ -217,8 +296,32 @@ var cubeGrid = ( function(
     automatons.map( traceSquare );
   }
 
+  me.toString = function( ) {
+    var str = "";
+    var x, y, n;
+    for(y = 0; y < xcount; y++ ) {
+      for(x = 0; x < ycount; x++ ) {
+	n = getNode( x, y );
+	if( n.coveringSquare !== null ) {
+	  str += 'X ';
+	} else if ( n.edgeSquares.length > 0 ) {
+	  str += '+ ';
+	} else {
+	  str += '  ';
+	}
+      }
+      str += '\n';
+    }
+    return str;
+  }
+
   return me;
 } )( 500, 500, 1, 100, 19 / 25, 3 / 4 );
+
+var maxSimTimeDelta = 100;
+var lastSimTime = performance.now( );
+var lastTime = lastSimTime;
+var delta = 0;
 
 cubeGrid.automatonCreatedCB = function( automaton ) {
   var cube = new THREE.Mesh( 
@@ -227,16 +330,21 @@ cubeGrid.automatonCreatedCB = function( automaton ) {
   );
   var m = new THREE.Matrix4();
   m.makeTranslation( 
-      automaton.x - 10, 
-      automaton.y - 10, 
+      automaton.x, 
+      automaton.y, 
       0 
   );
   cube.applyMatrix( m );
+  cube.name = automaton.colour.toString();
+  automaton.sceneId = cube.id;
   scene.add( cube );
 };
 
 cubeGrid.automatonMovedCB = function( automaton ) {
-
+  var cube = scene.getObjectById( automaton.sceneId );
+  cube.position.x = automaton.x;
+  cube.position.y = automaton.y;
+  cube.updateMatrix();
 };
 
 cubeGrid.createdSquareCB = function( square ) {
@@ -249,8 +357,8 @@ cubeGrid.createdSquareCB = function( square ) {
   );
   var m = new THREE.Matrix4();
   m.makeTranslation(
-      square.topLeft[ 0 ],
-      square.topLeft[ 1 ],
+      square.topLeft[ 0 ] + s/2,
+      square.topLeft[ 1 ] + s/2,
       0
   );
   cube.applyMatrix( m );
@@ -272,12 +380,22 @@ document.body.appendChild( renderer.domElement );
 
 camera.position.x = 250;
 camera.position.y = 250;
-camera.position.z = 500;
+camera.position.z = 250;
 
 cubeGrid.init( );
+//console.log( '\n' + cubeGrid.toString() );
 
-function render() {
+function render( timeStamp ) {
+  delta = timeStamp - lastSimTime; 
+  if( delta > maxSimTimeDelta ) {
+    cubeGrid.doSim( );
+    lastSimTime = timeStamp;
+  }
+  delta = timeStamp - lastTime;
+  lastTime = timeStamp;
+  //TODO: Other animations or effects
   requestAnimationFrame( render );
   renderer.render( scene, camera );
 }
-render();
+render( performance.now( ) );
+
